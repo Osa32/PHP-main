9022447ae35575811f9edf61aa04f1608c74a49a
Valentyn <forzaosa@gmail.com>
2015-07-21 12:58:22 +0300

    commit commit

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 <html>
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" href="style.css">
 	</head>
 
 	<body>
-</body>
+		
+	</body>
 </html>

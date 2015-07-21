c6961f3aff594f17d16cc9cdc934ad706fef5ad4
Valentyn <forzaosa@gmail.com>
2015-07-21 12:11:55 +0300

    аку
    куукук

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<p>Ще одніdsssssssssssssssssssssssss зміни</p>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+<html>
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	</head>
+
+	<body>
+</body>
+</html>

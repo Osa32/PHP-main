3a5fc30af2e810e86818cb2cb9a4f37be4fbb7a7
Valentyn Osovskyi <forzaosa@gmail.com>
2015-07-24 19:18:30 +0300

    24.07.15

--- LetComb/index.php
+++ LetComb/index.php
@@ -1,6 +1,16 @@
-<form action="processing.php" method="POST">
-	<textarea name="body.let" cols="100" rows="28"></textarea>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+<html>
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link href="style.css" rel="stylesheet" type="text/css" >
+	</head>
 
+	<body>
 
+		<form action="processing.php" method="POST">
+			<textarea name="body.let" cols="100" rows="28"></textarea>
 
-</form>
+		</form>
+
+	</body>
+</html>

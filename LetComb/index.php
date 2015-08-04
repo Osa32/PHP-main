dab1e4a03e423ada7d692186245c770e672131d2
Valentyn Osovskyi <forzaosa@gmail.com>
2015-08-04 20:09:21 +0300

    04.08.15

--- LetComb/index.php
+++ LetComb/index.php
@@ -8,7 +8,7 @@
 	<body>
 
 		<form action="processing.php" method="POST">
-			<textarea name="body.let" cols="100" rows="28">aaфффa</textarea>
+			<textarea name="body.let" cols="100" rows="28"></textarea>
 
 		</form>
 

c792ab8536365bfa9bf9ee5000062b274727fb5b
Valentyn Osovskyi <forzaosa@gmail.com>
2015-09-08 15:35:36 +0300

    08.09.15

--- OOP_1/function __clone.php
+++ OOP_1/function __clone.php
@@ -1,7 +1,7 @@
 <?php
 	class vvv
 	{
-		public $eeee=1;
+		public $eee=1;
 
 		function __clone()
 		{
@@ -10,9 +10,10 @@
 	}
 	
 		$a = new vvv();
-		$a->eee = 3;
+		//$a->eee = 3;
 		$a2 = clone $a;
 		
 		echo $a->eee;
 		echo $a2->eee;
+		//при виклику клонованих об'єктів - конструктор не викликається'
 ?>
---
+++ OOP_2/static.php
@@ -0,0 +1,22 @@
+<?php
+	class CookieLover {
+		static $loversCount = 55;
+		function __construct(){
+			++self::$loversCount."<br>";
+		}
+		static function welcome(){
+			echo "Це статичний метод";
+		}
+	}
+
+	$a = new CookieLover();
+	$b = new CookieLover();
+	$c = new CookieLover();
+
+	echo "Поточний баланс - ".CookieLover::$loversCount."<br>";
+	//до статичного методу/властивості можемо звертатись і не через клас
+
+	echo CookieLover::welcome();
+	//він належить тільки класу, а не об'єкту. Це метод, який є загальний для всіх
+	//так само, як константа - це властивість для всіх
+?>

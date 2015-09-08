b5165467594ff4ce4fda388a1f022972c9f4c5ec
Valentyn Osovskyi <forzaosa@gmail.com>
2015-09-08 09:45:52 +0300

    08.09.15

---
+++ My/return.php
@@ -0,0 +1,10 @@
+<?
+        //$a = NULL;
+        function myCount(){
+                $var = 1454;
+                echo $var;
+                return $var;
+        }
+        $a = myCount()*2;
+        echo $a;
+?>

731fb5fd8a4d2268bdcc268b4bcaa015c9a383e8
Valentyn <forzaosa@gmail.com>
2020-04-12 23:38:45 +0300

    Update 1. call_method_and property_through_method.php

--- OOP_1/1. call_method_and property_through_method.php
+++ OOP_1/1. call_method_and property_through_method.php
@@ -25,7 +25,19 @@
 		echo "Швидкість ".$this->a;
 		} 
 	}
-
+//----------------------------------
 $car1 = new Car();
 $car1->getSpeed();
 ?>
+
+class Car
+{
+  public $a = 2;
+
+  function Osa2 ($b) {
+    return "Швидкість ".$this->a." + ".$b;
+  }
+}
+
+$car1 = new Car;
+echo $car1->Osa2(300);

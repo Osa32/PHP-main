99ebf6610e2665b8edfe56fde19fa302854cb748
Valentyn <forzaosa@gmail.com>
2020-04-13 03:20:35 +0300

    Update exception.php

--- OOP_1/exception.php
+++ OOP_1/exception.php
@@ -1,6 +1,23 @@
 <?php
 
-//-------------------main version--------------------------------
+function inverse($x) {
+    if (!$x) {
+        throw new Exception('Деление на ноль.');
+    }
+    return 1/$x;
+}
+
+try {
+    echo inverse(5) . "\n";
+    echo inverse(0) . "\n";
+} catch (Exception $e) {
+    echo 'Выброшено исключение: ',  $e->getMessage(), "\n";
+}
+
+// Продолжение выполнения
+echo "Привет, мир\n";
+
+//---------------------------------------------------
 
 try
 	{
